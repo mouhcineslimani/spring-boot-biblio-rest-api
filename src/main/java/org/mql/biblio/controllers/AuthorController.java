83cde3a2491e1b8a0389283aa73fe9c50dbda8c0
Mouhcine Slimani <muhcine.slimani@gmail.com>
2023-06-02 21:10:08 +0100

    last commit

--- src/main/java/org/mql/biblio/controllers/AuthorController.java
+++ src/main/java/org/mql/biblio/controllers/AuthorController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.mql.biblio.models.Author;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,4 +66,21 @@ public class AuthorController {
 	}
 	
 	// Put et Delete mapping
+	
+	@PutMapping("/{id}")
+	public Author PutAuthor(@PathVariable int id,@RequestBody Author author)
+	{
+		authors.get(id).setName(author.getName());
+		authors.get(id).setYearBorn(author.getYearBorn()); 
+		return author;
+	}
+	
+	@DeleteMapping("/{id}")
+	public Author DeleteAuthor(@PathVariable int id)
+	{
+		Author a = authors.get(id);
+		authors.remove(id);
+		return a;
+	}
+	
 }
--- src/main/resources/static/js/fetch.js
+++ src/main/resources/static/js/fetch.js
@@ -9,7 +9,7 @@ function addAuthor() {
 	let options = {
 		method : 'POST',
 		body : JSON.stringify(data),
-		headers : {
+		headers : {vbbbbbbbbbbbbbbbbbb
 			'Content-Type' : 'application/json'
 		}
 	}
@@ -19,5 +19,43 @@ function addAuthor() {
 	.catch(err => console.log('Erreur :' + err))
 	
 }
+
+function putAuthor(id)
+{
+	let url = 'authors/'+id;
+	let result = document.getElementById('result');
+	let data = {
+		id : document.getElementById('id').value,
+		name : document.getElementById('name').value,
+		yearBorn : document.getElementById('yearBorn').value
+	}
+	let options = {
+		method : 'PUT',
+		body : JSON.stringify(data),
+		headers : {
+			'Content-Type' : 'application/json'
+		}
+	}
+	fetch(url, options)
+	    .then(response => response.json())
+		.then(data => result.innerHTML = data.name + ', bien modifié!!')
+		.catch(err => console.log('Erreur :' + err))
+}
+
+function putAuthor(id)
+{
+	let url = 'authors/'+id;
+	let result = document.getElementById('result');
+	let options = {
+			method : 'PUT', 
+			headers : {
+				'Content-Type' : 'application/json'
+			}
+		}
+	fetch(url, options)
+		.then(res => res.text()) // or res.json()
+		.then(res => console.log(res))
+}
+
 
 
